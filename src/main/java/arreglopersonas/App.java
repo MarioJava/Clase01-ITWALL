64e463a5090e239157bb5cbef754c70f70170e27
maliaga <marioaliaga79@gmail.com>
2016-03-15 21:21:01 -0300

    Ejemplo personas en erreglo

---
+++ src/main/java/arreglopersonas/App.java
@@ -0,0 +1,37 @@
+package arreglopersonas;
+
+/**
+ * Created by maliaga on 15-03-16.
+ */
+public class App {
+
+    public static void main(String... args) {
+        Persona p1 = new Persona();
+        p1.setEdad(10);
+        p1.setNombre("Mario");
+
+        Persona p2 = new Persona();
+        p2.setEdad(102);
+        p2.setNombre("Mario 2");
+
+        Persona p3 = new Persona();
+        p3.setEdad(103);
+        p3.setNombre("Mario 3");
+
+
+        Persona p4 = new Persona();
+        p4.setEdad(104);
+        p4.setNombre("Mario 4");
+
+        Persona p5 = new Persona();
+        p5.setEdad(105);
+        p5.setNombre("Mario 5");
+
+        Persona[] arr = {p1, p2, p3, p4, p5};
+
+        for (Persona p : arr) {
+            System.out.println("La edad es :" + p.getEdad());
+            System.out.println("EL nombre es " + p.getNombre());
+        }
+    }
+}
---
+++ src/main/java/arreglopersonas/Persona.java
@@ -0,0 +1,26 @@
+package arreglopersonas;
+
+/**
+ * Created by maliaga on 15-03-16.
+ */
+public class Persona {
+
+    private int edad;
+    private String nombre;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
