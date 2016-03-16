d546f68ef44b0d3d7871f05344f3c3fb6b235bdc
maliaga <marioaliaga79@gmail.com>
2016-03-15 21:23:31 -0300

    Ejemplo personas en erreglo fix lombok en Personas

--- src/main/java/arreglopersonas/Persona.java
+++ src/main/java/arreglopersonas/Persona.java
@@ -1,26 +1,15 @@
 package arreglopersonas;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by maliaga on 15-03-16.
  */
+@Setter
+@Getter
 public class Persona {
 
     private int edad;
     private String nombre;
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 }
