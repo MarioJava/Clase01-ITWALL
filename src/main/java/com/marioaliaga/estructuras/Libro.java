b675aa9a8a14dcd419ebd95aa6b86cc36ae3514c
Mario Aliaga Guzmán <maliaga@MacBookPro.local>
2016-08-10 21:06:04 -0400

    Listas enlazadas

---
+++ src/main/java/com/marioaliaga/estructuras/Libro.java
@@ -0,0 +1,16 @@
+package com.marioaliaga.estructuras;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Libro {
+
+    private String titulo;
+    private String autor;
+    private String isbn;
+
+}
